aa2dad5957ff62ea1fddcd0023918c2686303bc1
Abhishek korde <abhishekkorde44@gmail.com>
2022-10-18 16:28:00 -0400

    Sum of N number with Recursion

---
+++ src/Array/recursion2.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class recursion2 {
+    public static void main(String[] args) {
+        int num=5;
+        System.out.println(sumRec(num));
+    }
+    static int sumRec(int n){
+        //sum(n)=1+2+3+4...+ n-1 +n
+        //sum(n)= n + sum(n-1);
+        //sum of N number with Recursion.
+        if(n==1){
+            return 1;
+        }
+        else {
+            return n+ sumRec(n-1);
+        }
+    }
+}
